8df29b35960849c23d96a8b13bee641678e435ea
Mr.Salahinia <salahimahdi938@gmail.com>
2023-01-20 23:20:51 +0330

    Add project files.

---
+++ OCRTest/Program.cs
@@ -0,0 +1,21 @@
+﻿//// Initialize OCR engine
+//var recognitionEngine = new Aspose.OCR.AsposeOcr();
+//// Extract text from image
+//string result = recognitionEngine.RecognizeImage(@"G:\repository-open-graph-templateTest.png");
+//// Display the recognition result
+//Console.WriteLine(result);
+
+// NuGet PM> Install-Package IronOcr
+using IronOcr;
+using OCRTest;
+
+string imageText = new IronTesseract().Read(@"G:\repository-open-graph-templateTest.png").Text;
+
+var fixText = imageText.ToLower();
+
+if (!string.IsNullOrEmpty(fixText) && fixText == Constant.MyText.ToLower())
+{
+    Console.WriteLine(imageText);
+}
+
+Console.ReadKey();
